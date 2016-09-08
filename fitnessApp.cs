b13cd017543049e40ac4b22f04bb829c9ca0b5ea
Tunde Idahor <bidahor13@wou.edu>
2016-09-07 23:20:58 -0700

    changes made to the type declaration runningTotal

--- fitnessApp.cs
+++ fitnessApp.cs
@@ -1,9 +1,9 @@
- using System;
+  using System;
   class Program
   {
       static void Main()
       {
-        int runningTotal = 0;
+        double runningTotal = 0;
         bool keepGoing = true;
        //Prompts the user
        
@@ -13,17 +13,52 @@
         //Reads the users input
         string entry = Console.ReadLine();
           
-          if(entry == "quit"){
-            
+          if(entry.ToLower() == "quit"){
             keepGoing = false;
             
               } 
                else {
         
-        int minutes = int.Parse(entry);
+                 try{
+                   
+                   double minutes = double.Parse(entry);
+                   if(minutes <= 0)
+                 {
+                   Console.WriteLine( "The value " + minutes + " Minutes is not an acceptable value.");
+                   continue;
+                 }
+                   
+                    
+                 if(minutes <= 10 )
+                 {
+                    Console.WriteLine("Better than nothing, am I right?");
+                  }
+                 else if(minutes <= 30)
+                 {
+                   Console.WriteLine("Way to go hot stuff!! ");
+                 }
+                 else if(minutes <= 60)
+                 {
+                   Console.WriteLine("You must be a ninja warrior in training");
+                 }
+                 else
+                 {
+                   Console.WriteLine("ok!now you are just showing off!");
+                 }
+                   
+                   //Add minutes exercised to total
+                   runningTotal += minutes; 
         
-        //Add minutes exercised to total
-        runningTotal += minutes; 
+                   
+                 }
+                 catch( System.FormatException ) 
+                 {
+                   Console.WriteLine("This is not a valid input entry");
+                   continue;
+                 }
+                 
+                 
+                
         
         //Display total minutes exercised to the screen
         Console.Write("You have entered: "+ runningTotal + " minutes" + "\n");   
@@ -39,3 +74,6 @@
         
       }
    }
+  
+  
+  
